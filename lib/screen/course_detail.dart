c16731969d1a00b72e1880629f56fca89918a6c3
daviddprtma <davidchristianpratama@gmail.com>
2022-09-15 21:29:57 +0700

    homework week 3

--- lib/screen/course_detail.dart
+++ lib/screen/course_detail.dart
@@ -1,8 +1,76 @@
 import 'package:flutter/material.dart';
 
 class CourseDetail extends StatelessWidget {
+  String courses = '';
+  String kp = "";
+  String hari = "";
+  String lokasi = "";
+  String sks = "";
+
+  CourseDetail(this.courses, this.kp, this.hari, this.lokasi, this.sks);
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail Course"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(courses),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 50,
+                height: 50,
+                margin: EdgeInsets.only(top: 15),
+                color: Colors.white,
+                child: Card(
+                    child: Text(
+                  kp,
+                  textAlign: TextAlign.center,
+                )),
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                margin: EdgeInsets.only(top: 15),
+                color: Colors.white,
+                child: Card(
+                    child: Text(
+                  hari,
+                  textAlign: TextAlign.center,
+                )),
+              ),
+              Container(
+                width: 55,
+                height: 55,
+                margin: EdgeInsets.only(top: 15),
+                color: Colors.white,
+                child: Card(
+                    child: Text(
+                  lokasi,
+                  textAlign: TextAlign.center,
+                )),
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                margin: EdgeInsets.only(top: 15),
+                color: Colors.white,
+                child: Card(
+                  child: Text(
+                    sks,
+                    textAlign: TextAlign.center,
+                  ),
+                  shadowColor: Colors.black,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screen/my_courses.dart
+++ lib/screen/my_courses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/screen/course_detail.dart';
 
 class MyCourses extends StatelessWidget {
   @override
@@ -6,6 +7,135 @@ class MyCourses extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Course"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Align(alignment: Alignment.center),
+          Container(
+              width: 100,
+              height: 100,
+              margin: EdgeInsets.only(top: 10),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/me.jpg")),
+                  shape: BoxShape.circle)),
+          Container(
+            child: Text(
+              "David Pratama",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(child: Text("160419103"), margin: EdgeInsets.only(top: 5)),
+          Container(
+            child: Text("Informatika"),
+          ),
+          Container(
+            child: Text("Gasal 2022-2023"),
+          ),
+          Container(
+            width: 600,
+            height: 600,
+            margin: EdgeInsets.only(top: 20),
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CourseDetail(
+                                "Numerical Method",
+                                "KP A",
+                                "Kamis 10.40",
+                                "TF 03.01",
+                                "2 sks")));
+                  },
+                  child: Text("NuMeth (A)"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CourseDetail(
+                                "Research Methodology",
+                                "KP ZC",
+                                "Senin 09.45",
+                                "TF 04.02",
+                                "3 sks")));
+                  },
+                  child: Text("RM (ZC)"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CourseDetail("Kerja Praktek",
+                                "KP MA", "Senin 19.25", "-", "2 sks")));
+                  },
+                  child: Text("KP (MA)"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CourseDetail(
+                                "Management Information Systems",
+                                "KP -",
+                                "Selasa 13.00",
+                                "TF 02.05",
+                                "3 sks")));
+                  },
+                  child: Text("MIS (-)"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CourseDetail(
+                                "Emerging Technology",
+                                "KP B",
+                                "Senin 07.00",
+                                "TC 04C",
+                                "3 sks")));
+                  },
+                  child: Text("EmerTech (B)"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CourseDetail(
+                                "Literasi Digital",
+                                "KP E5",
+                                "Rabu 15.45",
+                                "TB 01.01C",
+                                "2 sks")));
+                  },
+                  child: Text("LDI (E5)"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => CourseDetail(
+                                "IS Evolving Technology",
+                                "KP A",
+                                "Kamis 13.00",
+                                "TF 02.04",
+                                "3 sks")));
+                  },
+                  child: Text("ISET(A)"),
+                )
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
